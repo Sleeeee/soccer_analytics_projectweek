28676b24f1fbbb32f5dfeff800e7702634dbaa37
Sleeeee <jstasjustin@gmail.com>
2025-04-01 15:44:57 +0200

    Clean up main.py workflow

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import psycopg2
 import dotenv
 
-from phases import get_phase_timestamps, get_transition_timestamps
+from phases import get_phase_timestamps, get_transition_timestamps, get_query_between_timestamps
 
 def main():
     dotenv.load_dotenv()
@@ -25,10 +25,22 @@ def main():
         sslmode="require",
     )
 
-    print(get_phase_timestamps("out_of_possession", "Dender", get_transition_timestamps('5uts2s7fl98clqz8uymaazehg', conn)))
+    MATCH_ID = "5uts2s7fl98clqz8uymaazehg"
+    PHASE_NAME = "out_of_possession"
+    TEAM_NAME = "Dender"
+    BASE_QUERY = """
+        SELECT et.name, p.player_id, p.player_name, p.team_id, me.match_id, me.timestamp, me.end_timestamp
+        FROM players p
+        JOIN matchevents me ON p.player_id = me.player_id
+        JOIN eventtypes et ON me.eventtype_id = et.eventtype_id
+        WHERE me.match_id = '5uts2s7fl98clqz8uymaazehg' 
+    """
 
-    mps.field("green", figsize=8)
-    plt.show()
+    timestamps = get_transition_timestamps(MATCH_ID, conn)
+    phase_timestamps = get_phase_timestamps(PHASE_NAME, TEAM_NAME, timestamps)
+    phase_query = get_query_between_timestamps(BASE_QUERY, phase_timestamps, conn)
+    phase_df = pd.read_sql_query(phase_query, conn)
+    print(phase_df.head())
 
 if __name__ == "__main__":
     main()
