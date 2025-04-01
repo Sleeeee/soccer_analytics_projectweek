cb41006e80cfe9246c6ec0609627f309e4bf9d6f
Sleeeee <jstasjustin@gmail.com>
2025-04-01 09:49:33 +0200

    Add initial main

---
+++ main.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotsoccer as mps
+import pandas as pd
+
+def main():
+    mps.field("green", figsize=8)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
