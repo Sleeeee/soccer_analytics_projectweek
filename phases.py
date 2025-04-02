012349e095471691f4105c75f15a2c1a12f6f9b1
Swekka <swekka@proton.me>
2025-04-02 14:41:14 +0200

    add match visualisation

--- phases.py
+++ phases.py
@@ -4,33 +4,36 @@ def get_transition_timestamps(match_id, conn):
     query = f"""
     SELECT
         event_id,
+        period_id,
         timestamp,
-        -- end_timestamp,
         ball_owning_team,
         t.team_name
     FROM matchevents e
     JOIN
         teams t ON e.ball_owning_team = t.team_id
     WHERE e.match_id = '{match_id}'
+    ORDER BY period_id, timestamp
     """
 
     events_df = pd.read_sql_query(query, conn)
     current_timestamp = events_df.iloc[0]['timestamp']
     current_team = events_df.iloc[0]['team_name']
+    current_period = events_df.iloc[0]['period_id']
     phase_list = []
     for i, row in events_df.iterrows():
         if i == 0: continue
         if row['team_name'] == current_team:
             continue
-        phase_list.append({'team': current_team, 'timestamp':  pd.Timedelta(current_timestamp).total_seconds(), 'original_timestamp': current_timestamp})
+        phase_list.append({'team': current_team, 'timestamp': pd.Timedelta(current_timestamp).total_seconds(), 'original_timestamp': current_timestamp, 'period_id': current_period})
         current_team = row['team_name']
         current_timestamp = row['timestamp']
+        current_period = row['period_id']
 
     phase_list2 = []
     for i in range(len(phase_list)):
         if i == len(phase_list)-1:
             break
-        
+
         if phase_list[i+1]['timestamp'] - phase_list[i]['timestamp'] > 5:
             phase = phase_list[i]
             phase['possession'] = False
@@ -71,22 +74,21 @@ def get_phase_timestamps(phase_name: str, team_name: str, timestamps: list):
     for i in range(len(timestamps) - 1):
         current_event = timestamps[i]
         next_event = timestamps[i+1]
-        if (target_phase["team"] == current_event["team"] or target_phase["team"] is None) and target_phase["possession"] == current_event["possession"]:
+        if (target_phase["team"] == current_event["team"] or target_phase["team"] is None) and target_phase["possession"] == current_event["possession"] and current_event["period_id"] == next_event["period_id"]:
             filtered.append({
                 "timestamp": current_event["original_timestamp"],
+                "period_id": current_event["period_id"],
                 "end_timestamp": next_event["original_timestamp"]
             })
     return filtered
 
-
-def get_query_between_timestamps(query, filtered_data, conn) :
+def get_query_between_timestamps(query, filtered_data) :
     query += 'AND ('
 
     timestamp_conditions = []
     for entry in filtered_data:
         start_time = entry['timestamp']
         end_time = entry["end_timestamp"]
-        
         condition = f"(me.timestamp BETWEEN '{start_time}' AND '{end_time}')"
         timestamp_conditions.append(condition)
 
@@ -94,3 +96,18 @@ def get_query_between_timestamps(query, filtered_data, conn) :
     query += ")"
 
     return query
+
+def get_player_tracking(match_id, conn):
+    query = f"""
+    SELECT
+        x,
+        y,
+        pt.player_id,
+        frame_id,
+        team_id
+    FROM player_tracking pt
+    JOIN players p ON pt.player_id = p.player_id
+    WHERE game_id = '{match_id}'
+    ORDER BY period_id, timestamp, player_id
+    """
+    return pd.read_sql_query(query, conn)
