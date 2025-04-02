1ebd1201053e7a1dce8eb46bea1c61281fe68161
Sleeeee <jstasjustin@gmail.com>
2025-04-02 15:32:54 +0200

    Add heatmap plotting

--- field.py
+++ field.py
@@ -1,22 +1,39 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import mplsoccer
+import scipy.ndimage
 
-def get_quadrant_coordinates(quadrant_id: int):
-    x = (((quadrant_id % 3) * 1/3) + 1/6) * 100
-    y = (((quadrant_id // 3) * 1/3) + 1/6) * 100
-    return {"x": x, "y": y}
+from phases import get_transition_timestamps, get_phase_timestamps, get_query_between_timestamps
 
-def calculate_distance(x1, y1, x2, y2):
-    return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+def get_heatmap(match_id: str, team_name: str, phase_name: str, conn, fig, ax):
+    BASE_QUERY = f"""
+        SELECT me.timestamp, me.end_timestamp, me.x, me.y
+        FROM matchevents me
+        JOIN teams t ON me.team_id = t.team_id
+        WHERE me.match_id = '{match_id}' AND t.team_name = '{team_name}'
+    """
+    timestamps = get_transition_timestamps(match_id, conn)
+    phase_timestamps = get_phase_timestamps(phase_name, team_name, timestamps)
+    phase_query = get_query_between_timestamps(BASE_QUERY, phase_timestamps, conn)
+    phase_df = pd.read_sql_query(phase_query, conn)
+    print(phase_query)
 
-def get_closest_quadrant(ball_coords):
-    distances = []
-    for i in range(9):
-        quadrant = get_quadrant_coordinates(i)
-        distance = calculate_distance(ball_coords["x"], ball_coords["y"], quadrant["x"], quadrant["y"])
-        distances.append(distance)
-    return np.argmin(distances)
-
-def get_ball_quadrant(events_df: pd.DataFrame):
-    events_df["closest_quadrant"] = events_df.apply(lambda row: get_closest_quadrant({"x": row["x"], "y": row["y"]}), axis=1)
-    return events_df.drop(columns=["x", "y"])
+    pitch = mplsoccer.Pitch(pitch_type='statsbomb', line_zorder=2, pitch_color='#22312b', line_color='#efefef')
+    pitch.draw(ax=ax, figsize=(6.6, 4.125))
+    bin_statistic = pitch.bin_statistic(phase_df.x, phase_df.y, statistic='count', bins=(25, 25))
+    bin_statistic['statistic'] = scipy.ndimage.gaussian_filter(bin_statistic['statistic'], 1)
+    pcm = pitch.heatmap(bin_statistic, ax=ax, cmap='hot', edgecolors='#22312b')
+    fig.set_facecolor('#22312b')
+    cbar = fig.colorbar(pcm, ax=ax, shrink=0.6)
+    cbar.outline.set_edgecolor('#efefef')
+    cbar.ax.yaxis.set_tick_params(color='#efefef')
+    ticks = plt.setp(plt.getp(cbar.ax.axes, 'yticklabels'), color='#efefef')
+    
+def get_heatmaps(match_id: str, phase_name: str, conn):
+    HOME_TEAM_NAME = "Dender"
+    AWAY_TEAM_NAME = "Gent"
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    get_heatmap(match_id, HOME_TEAM_NAME, phase_name, conn, fig, ax1)
+    get_heatmap(match_id, AWAY_TEAM_NAME, phase_name, conn, fig, ax2)
+    plt.show()
